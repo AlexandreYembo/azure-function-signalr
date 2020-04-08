440d88b7b4e550683abe802b725be2ae5d305605
Andre Ferreira Lima <andre.lima@grupoltm.com.br>
2020-04-08 13:32:33 -0300

    negotiate fun name :zap:

--- ServerlessPoc.Tests/FunctionsTest.cs
+++ ServerlessPoc.Tests/FunctionsTest.cs
@@ -9,75 +9,68 @@ using Moq;
 using Newtonsoft.Json;
 using NUnit.Framework;
 
-namespace ServerlessPoc.Tests
-{
-    public class Tests
-    {
+namespace ServerlessPoc.Tests {
+    public class Tests {
         private Mock<IAsyncCollector<SignalRMessage>> _collectorMock;
-        private readonly ILogger logger = TestFactory.CreateLogger();
+        private readonly ILogger logger = TestFactory.CreateLogger ();
 
         [SetUp]
-        public void Setup()
-        {
-            _collectorMock = new Mock<IAsyncCollector<SignalRMessage>>();
+        public void Setup () {
+            _collectorMock = new Mock<IAsyncCollector<SignalRMessage>> ();
         }
 
         [Test]
-        public void Should_Return_BadRequest_Invalid_Message()
-        {
-            var request = TestFactory.CreateHttpRequest("name", "value");
+        public void Should_Return_BadRequest_Invalid_Message () {
+            var request = TestFactory.CreateHttpRequest ("name", "value");
 
-            var result = ServerlessPoc.Function.ServerlessRealtime.SendMessage(request, _collectorMock.Object, logger).Result;
+            var result = ServerlessPoc.Function.ServerlessRealtime.SendMessage (request, _collectorMock.Object, logger).Result;
 
-            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+            Assert.IsInstanceOf<BadRequestObjectResult> (result);
         }
 
         [Test]
-        public void ShouldReturn_ValidRequest_SendMessage()
-        {
-            var data = JsonConvert.SerializeObject(Stub.GetMessageRequest());
+        public void ShouldReturn_ValidRequest_SendMessage () {
+            var data = JsonConvert.SerializeObject (Stub.GetMessageRequest ());
 
-            var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(data));
+            var stream = new MemoryStream (System.Text.Encoding.UTF8.GetBytes (data));
 
-            var baseRequest = new DefaultHttpContext();
+            var baseRequest = new DefaultHttpContext ();
             baseRequest.Request.Body = stream;
             baseRequest.Request.ContentLength = stream.Length;
 
-            var request = new DefaultHttpRequest(baseRequest);
+            var request = new DefaultHttpRequest (baseRequest);
 
-            var result = ServerlessPoc.Function.ServerlessRealtime.SendMessage(request, _collectorMock.Object, logger).Result;
+            var result = ServerlessPoc.Function.ServerlessRealtime.SendMessage (request, _collectorMock.Object, logger).Result;
 
-            Assert.IsInstanceOf<NoContentResult>(result);
+            Assert.IsInstanceOf<NoContentResult> (result);
         }
 
         [Test]
-        public void ShouldReturn_ValidRequest_Send_Vote()
-        {
-            var data = JsonConvert.SerializeObject(Stub.GetVoteRequest());
+        public void ShouldReturn_ValidRequest_Send_Vote () {
+            var data = JsonConvert.SerializeObject (Stub.GetVoteRequest ());
 
-            var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(data));
+            var stream = new MemoryStream (System.Text.Encoding.UTF8.GetBytes (data));
 
-            var baseRequest = new DefaultHttpContext();
+            var baseRequest = new DefaultHttpContext ();
             baseRequest.Request.Body = stream;
             baseRequest.Request.ContentLength = stream.Length;
 
-            var request = new DefaultHttpRequest(baseRequest);
+            var request = new DefaultHttpRequest (baseRequest);
 
-            var result = ServerlessPoc.Function.ServerlessRealtime.SendMessage(request, _collectorMock.Object, logger).Result;
+            var result = ServerlessPoc.Function.ServerlessRealtime.SendMessage (request, _collectorMock.Object, logger).Result;
 
-            Assert.IsInstanceOf<NoContentResult>(result);
+            Assert.IsInstanceOf<NoContentResult> (result);
         }
 
         [Test]
-        public void Should_Return_Connection_From_SignalR()
-        {
-            var request = TestFactory.CreateHttpRequest("name", "value");
+        public void Should_Return_Connection_From_SignalR () {
+            var request = TestFactory.CreateHttpRequest ("name", "value");
 
-            var connection = new SignalRConnectionInfo();
+            var connection = new SignalRConnectionInfo ();
 
-            var result = ServerlessPoc.Function.Negotiate.NegotiateFunc(request, connection);
+            var result = ServerlessPoc.Function.NegotiateFunc.Negotiate (request, connection);
 
-            Assert.IsInstanceOf<SignalRConnectionInfo>(result);
+            Assert.IsInstanceOf<SignalRConnectionInfo> (result);
         }
     }
 }
--- ServerlessPoc/Negotiate.cs
+++ ServerlessPoc/Negotiate.cs
@@ -1,23 +1,19 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 
-namespace ServerlessPoc.Function
-{
-    public static class Negotiate
-    {
-        [FunctionName("negotiate")]
-        public static SignalRConnectionInfo NegotiateFunc(
-        [HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req,
-        [SignalRConnectionInfo(HubName = "chat")]SignalRConnectionInfo connectionInfo)
-        {
+namespace ServerlessPoc.Function {
+    public static class NegotiateFunc {
+        [FunctionName ("negotiate")]
+        public static SignalRConnectionInfo Negotiate (
+            [HttpTrigger (AuthorizationLevel.Anonymous)] HttpRequest req, [SignalRConnectionInfo (HubName = "chat")] SignalRConnectionInfo connectionInfo) {
             return connectionInfo;
         }
     }
